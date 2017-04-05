18c1f3a23ad2673741cd6193abdd3f59a79892a3
huanghaixin008 <huanghaixin008@users.noreply.github.com>
2017-04-05 13:57:43 +0800

    LRU cache bug fix

--- linux-3.12.49/mm/swap.c
+++ linux-3.12.49/mm/swap.c
@@ -817,10 +817,11 @@ void release_pages(struct page **pages, int nr, bool cold)
 
 	for (i = 0; i < nr; i++) {
 		struct page *page = pages[i];
+		/*
 		if(page_zone(page)->name[0] == 'S' && page_zone(page)->name[1] == 'c' && page_zone(page)->name[2] == 'm') {
 				//daisy_printk("find scm page, continue\n");
 				continue;
-		}
+		} */
 		if (unlikely(PageCompound(page))) {
 			if (zone) {
 				spin_unlock_irqrestore(&zone->lru_lock, flags);
