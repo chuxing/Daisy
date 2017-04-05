f5912dbadf745391d8d8ac6c02421f853fb0949e
huanghaixin008 <huanghaixin008@users.noreply.github.com>
2017-04-05 14:14:40 +0800

    foreach zonelist bug fix

--- linux-3.12.49/mm/mmzone.c
+++ linux-3.12.49/mm/mmzone.c
@@ -50,15 +50,15 @@ static inline int zref_in_nodemask(struct zoneref *zref, nodemask_t *nodes)
 	return 1;
 #endif /* CONFIG_NUMA */
 }
-
-/* special design for scm: if scm just ignore zonelist mechanism */
+/*
+// special design for scm: if scm just ignore zonelist mechanism
 static int zone_ok_or_scm(struct zoneref *z, enum zone_type highest_zoneidx) {
 	if (highest_zoneidx != ZONE_SCM) {
 		return zonelist_zone_idx(z) > highest_zoneidx;
 	} else {
 		return zonelist_zone_idx(z) != ZONE_SCM;
 	}
-}
+}*/
 
 
 /* Returns the next zone at or below highest_zoneidx in a zonelist */
@@ -72,10 +72,10 @@ struct zoneref *next_zones_zonelist(struct zoneref *z,
 	 * Only filter based on nodemask if it's set
 	 */
 	if (likely(nodes == NULL))
-		while (zone_ok_or_scm(z, highest_zoneidx))
+		while (zonelist_zone_idx(z) > highest_zoneidx))
 			z++;
 	else
-		while (zone_ok_or_scm(z, highest_zoneidx) ||
+		while (zonelist_zone_idx(z) > highest_zoneidx) ||
 				(z->zone && !zref_in_nodemask(z, nodes)))
 			z++;
 
